25cce7ac7d3a467f99fb66a8253933fc6ae3d11f
Chenjl2020522010 <110597916+Chenjl2020522010@users.noreply.github.com>
2023-05-18 20:07:06 +0800

    Add files via upload

--- a.py
+++ a.py
@@ -1,107 +1,123 @@
+import tkinter as tk
 import random
-import time
-import os
 
-# 游戏窗口的宽度和高度
-window_width = 30
-window_height = 20
+# 定义地图和障碍物
+MAP_WIDTH = 500
+MAP_HEIGHT = 500
+OBSTACLES = [(100, 99), (200, 200), (300, 300), (400, 400)]
 
-# 玩家初始位置和移动速度
-player_x = 1
-player_y = 1
-player_speed = 1
+path = []
+for i in range(100,300):
+    OBSTACLES.append((100,i))
+    OBSTACLES.append((i,100))
+'''
+targetaction = [(-1,-1),(-1,0),(0,0),(1,0),(0,-1),(0,1),(1,-1),(1,1)]
+for i in range(50):
+    path.append(targetaction[random.randint(0,7)])
+print(path)
+'''
+# target 沿固定路径移动
+path = [(0, 0), (0, -1), (0, 1), (0, 0), (1, 1), (-1, 0), (1, -1), (0, 1), (1, 1), (0, 0), (0, 0), (1, 1), (-1, -1), (0, -1), (1, 1), (0, -1), (1, 0), (-1, 0), (1, 0), (-1, 0), (0, 0), (0, 1), (1, 0), (-1, 0), (-1, -1), (1, 1), (1, 1), (-1, -1), (0, -1), (1, 0), (-1, 0), (-1, 0), (1, 1), (1, 0), (-1, -1), (0, -1), (0, -1), (1, 1), (-1, 0), (0, -1), (1, -1), (-1, 0), (1, -1), (-1, 0), (1, 0), (1, 0), (1, 1), (1, 1), (0, 1), (1, 0)]
 
-# 敌方初始位置和移动速度
-enemy_x = window_width - 2
-enemy_y = window_height - 2
-enemy_speed = 1
 
-# 子弹列表
-bullets = []
+# 定义智能体和目标
+AGENT_RADIUS = 10
+AGENT_COLOR = 'blue'
+TARGET_RADIUS = 10
+TARGET_COLOR = 'red'
 
-# 食物列表
-foods = []
-food_count = 10
+class Agent:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+        self.radius = AGENT_RADIUS
+        self.color = AGENT_COLOR
 
-# 创建食物
-for _ in range(food_count):
-    food_x = random.randint(1, window_width - 2)
-    food_y = random.randint(1, window_height - 2)
-    foods.append((food_x, food_y))
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
 
-# 游戏结束标志
-game_over = False
+    def draw(self, canvas):
+        canvas.create_oval(self.x - self.radius, self.y - self.radius,
+                           self.x + self.radius, self.y + self.radius,
+                           fill=self.color)
 
-# 游戏循环
-while not game_over:
-    # 清空终端窗口
-    os.system('cls' if os.name == 'nt' else 'clear')
+class Target:
+    def __init__(self, x, y, path):
+        self.x = x
+        self.y = y
+        self.radius = TARGET_RADIUS
+        self.color = TARGET_COLOR
+        self.step = 0
+        self.path = path
 
-    # 绘制游戏界面
-    for y in range(window_height):
-        for x in range(window_width):
-            if (x, y) == (player_x, player_y):
-                print('P', end='')
-            elif (x, y) == (enemy_x, enemy_y):
-                print('E', end='')
-            elif (x, y) in bullets:
-                print('*', end='')
-            elif (x, y) in foods:
-                print('F', end='')
-            else:
-                print(' ', end='')
-        print()
 
-    # 获取键盘输入
-    direction = input("请输入移动方向（w上，s下，a左，d右）：")
+    def draw(self, canvas):
+        canvas.create_oval(self.x - self.radius, self.y - self.radius,
+                           self.x + self.radius, self.y + self.radius,
+                           fill=self.color)
+    def move(self, path):
+        if self.step > 5000:
+            return
+        self.x += path[self.step % 50][0]
+        self.y += path[self.step % 50][1]
+        self.step += 1
 
-    # 更新玩家位置
-    if direction == 'w':
-        player_y -= player_speed
-    elif direction == 's':
-        player_y += player_speed
-    elif direction == 'a':
-        player_x -= player_speed
-    elif direction == 'd':
-        player_x += player_speed
+# 实现智能体的移动策略
+def move_agent(agent, target):
+    dx = target.x - agent.x
+    dy = target.y - agent.y
+    dist = (dx ** 2 + dy ** 2) ** 0.5
+    if dist > 0:
+        agent.vx = dx / dist
+        agent.vy = dy / dist
+    else:
+        agent.vx = 0
+        agent.vy = 0
 
-    # 更新敌方位置
-    if enemy_x > player_x:
-        enemy_x -= enemy_speed
-    elif enemy_x < player_x:
-        enemy_x += enemy_speed
-    if enemy_y > player_y:
-        enemy_y -= enemy_speed
-    elif enemy_y < player_y:
-        enemy_y += enemy_speed
+    # 检查是否碰到障碍物
+    for obstacle in OBSTACLES:
+        if (agent.x - obstacle[0]) ** 2 + (agent.y - obstacle[1]) ** 2 < (AGENT_RADIUS + 5) ** 2:
+            agent.vx = -agent.vx
+            agent.vy = -agent.vy
+            break
 
-    # 敌方发射子弹
-    if random.randint(0, 100) < 10:
-        bullet = (enemy_x, enemy_y)
-        bullets.append(bullet)
+    agent.move()
+    target.move(path)
 
-    # 移动子弹
-    bullets = [(x, y-1) for (x, y) in bullets]
 
-    # 检测碰撞
-    if (player_x, player_y) in bullets:
-        game_over = True
-    bullets = [(x, y) for (x, y) in bullets if y > 0]
+# 实现智能体和目标的交互
+def update(agent, target, canvas):
+    move_agent(agent, target)
+    canvas.delete('all')
+    for obstacle in OBSTACLES:
+        canvas.create_rectangle(obstacle[0] - 5, obstacle[1] - 5,
+                                obstacle[0] + 5, obstacle[1] + 5,
+                                fill='gray')
+    agent.draw(canvas)
+    target.draw(canvas)
 
-    # 检查是否吃到食物
-    if (player_x, player_y) in foods:
-        foods.remove((player_x, player_y))
+# 用tkinter展示效果
+def main():
+    root = tk.Tk()
+    root.title('Agent Tracking Target')
+    canvas = tk.Canvas(root, width=MAP_WIDTH, height=MAP_HEIGHT)
+    canvas.pack()
 
-    # 判断游戏是否结束
-    if len(foods) == 0:
-        game_over = True
+    # 初始化智能体和目标
+    agent = Agent(MAP_WIDTH // 2, MAP_HEIGHT // 2)
+    #target = Target(random.randint(0, MAP_WIDTH), random.randint(0, MAP_HEIGHT))
+    target = Target(50, 50, path)
 
-    # 控制游戏帧率
-    time.sleep(0.1)
+    # 更新画面
+    def update_canvas():
+        update(agent, target, canvas)
+        root.after(10, update_canvas)
 
-# 游戏结束，显示结果
-os.system('cls' if os.name == 'nt' else 'clear')
-if len(foods) == 0:
-    print("恭喜你，吃完了所有的食物！")
-else:
-    print("游戏结束，你被击中了！")
+    update_canvas()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
