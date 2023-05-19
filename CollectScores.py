78efccd75f47f670c8eca6ed0c0740b6894da376
liupch1 <liupch1@shanghaitech.edu.cn>
2023-05-20 07:54:59 +0800

    parallel

--- CollectScores.py
+++ CollectScores.py
@@ -1,17 +1,35 @@
 import subprocess
+import multiprocessing
 
 NUM_RUNS = 100
-def collect_score(score,scores):
+
+def collect_score(score, scores):
     scores.append(score)
-    
+
+def run_game(output_queue):
+    output_score = subprocess.check_output(['python', 'MainGame.py'])
+    score = int(output_score.decode().strip())
+    output_queue.put(score)
+
 if __name__ == '__main__':
-    NUM_RUNS = 100
     scores = []
-    for count in range(NUM_RUNS):
-        output_score = subprocess.check_output(['python', 'MainGame.py'])
-        score = int(output_score.decode().strip())
-        scores.append(score)
+    output_queue = multiprocessing.Queue()
+
+    processes = []
+    for i in range(NUM_RUNS):
+        
+        print(f"Running game {i+1}/{NUM_RUNS}",end='\r',flush=True)
+        process = multiprocessing.Process(target=run_game, args=(output_queue,))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    while not output_queue.empty():
+        score = output_queue.get()
+        collect_score(score, scores)
+
     average_score = sum(scores) / len(scores)
-    print("average score:",average_score)   
-    print("scores[%d]:",count)
-    print(scores)
+    print("\nAverage score:", average_score)
+    print("\nScores:\n", scores)
